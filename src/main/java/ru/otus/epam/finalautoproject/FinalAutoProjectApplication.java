875a8ea9315ebe82c19f8f410f12592fc27d2ed9
Tatiana <tokio9507@mail.ru>
2020-05-11 11:37:35 +0300

    Initial commit

---
+++ src/main/java/ru/otus/epam/finalautoproject/FinalAutoProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.epam.finalautoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAutoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalAutoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/otus/epam/finalautoproject/FinalAutoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.epam.finalautoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalAutoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
